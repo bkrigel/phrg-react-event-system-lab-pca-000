e0abeb677efcc12f676de5ec445d18d44655c9a2
Benjamin Krigel <benjamin.krigel@gmail.com>
2018-10-02 22:17:08 -0400

    my focus is getting blurry

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+
+  focused = () => {
+    console.log('Good!');
+  }
+
+  distracted = () => {
+    console.log('Hey! Eyes on me!');
+  }
+
+  render() {
+    return (
+      <button onFocus={this.focused} onBlur={this.distracted} type="password" />
+    )
+  }
+}
+
+export default EyesOnMe;
